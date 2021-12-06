2a190d1124b0ab1d9ab0bf37cffc0118a759154c
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2021-12-06 16:45:57 +0100

    création de entité auteur

---
+++ src/Entity/Author.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ */
+class Author
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $denthDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getDenthDate(): ?\DateTimeInterface
+    {
+        return $this->denthDate;
+    }
+
+    public function setDenthDate(\DateTimeInterface $denthDate): self
+    {
+        $this->denthDate = $denthDate;
+
+        return $this;
+    }
+}
